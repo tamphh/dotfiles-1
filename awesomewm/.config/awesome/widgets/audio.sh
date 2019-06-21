4bd7f3a6552aea44d379f6ee5e24bf5145d843b5
szorfein <szorfein@protonmail.com>
2019-06-22 00:16:10 +1200

    audio.sh - tested on arch #7

--- awesomewm/.config/awesome/widgets/audio.sh
+++ awesomewm/.config/awesome/widgets/audio.sh
@@ -2,12 +2,30 @@
 
 audio_card=""
 volume=""
-AMIXER=$(which amixer)
-MPC=$(which mpc)
+alsa=""
+pulse=""
+log_file="/tmp/audio.log"
+log=false # true or false
+time=$(date)
+
+log() {
+  if [ $log ] ; then
+    echo "$time - $1" >> $log_file
+  fi
+}
 
 die() {
-  echo "Error: $1"
+  log "[DIE]: $1"
   exit 1
+}
+
+detect_bin() {
+  alsa=$(which amixer 2>/dev/null 2>&1)
+  pulse=$(which pactl 2>/dev/null 2>&1)
+  log "detect_bin - alsa : $alsa , pulse : $pulse"
+  if [[ ! -x $alsa ]] && [[ ! -x $pulse ]] ; then
+	  die "detect_bin - fail to detect amixer or pulse"
+  fi
 }
 
 check_audio_card() {
@@ -17,25 +35,47 @@ check_audio_card() {
   fi
 }
 
-alsa_or_pulse() {
-  local bin pulse alsa
-  #if bin=$(which pactl 2>/dev/null) && volume=$($bin list sinks | grep "^Volume" | grep -o -E "[[:digit]]+%" | head -n 1) ; then
-  #  echo "pactlPulse$1: $volume"
-  pulse=$($AMIXER -c $1 -M -D pulse sget Master 2>/dev/null | grep -o -E "[[:digit:]]+%" | head -n 1)
-  alsa=$($AMIXER -c $1 -M sget Master 2>/dev/null | grep -o -E "[[:digit:]]+%" | head -n 1)
-  if [ ! -z $pulse ] ; then
-    echo "amixerPulse$1: $pulse"
-    return 0
-  elif [ ! -z $alsa ] ; then
-    echo "amixerAlsa$1: $alsa"
+for_pulse() {
+  local sink
+  log "Attemp with pulse $pulse"
+  sink=$($pulse list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,')
+  volume=$($pulse list sinks | grep '^[[:space:]]Volume:' | head -n $(( $sink + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+  if [[ ! -z $sink ]] && [[ ! -z $volume ]] ; then
+    echo "pactlPulse$1: $volume%"
     return 0
   fi
-  return 1
+  die "Card no found with pulse s:$sink - n:$now -a:$1"
+}
+
+for_alsa() {
+  local p a
+  log "Attemp with alsa $alsa"
+  p=$($alsa -c $1 -M -D pulse sget Master 2>/dev/null | grep -o -E "[[:digit:]]+%" | head -n 1)
+  a=$($alsa -c $1 -M get Master 2>/dev/null | grep -o -E "[[:digit:]]+%" | head -n 1)
+
+  if [ ! -z $p ] ; then
+    echo "amixerPulse$1: $p"
+    return 0
+  elif [ ! -z $a ] ; then
+    echo "amixerAlsa$1: $a"
+    return 0
+  fi
+  die "ALSA - No found with $alsa and $1 , p:<<  $p >>, a:<< $a >>"
+}
+
+alsa_or_pulse() {
+  if [[ -x $pulse ]] ; then
+    for_pulse
+  elif [[ -x $alsa ]] ; then
+    for_alsa $1
+  else
+	  die "No found $alsa or $pulse"
+  fi
 }
 
 # Check first a card given as argument else try card n2 or n1
 show_volume() {
-  [ -z $AMIXER ] && die "amixer no found"
+	detect_bin
   if [ ! -z $audio_card ] ; then
     alsa_or_pulse $audio_card
   elif alsa_or_pulse 2 ; then
