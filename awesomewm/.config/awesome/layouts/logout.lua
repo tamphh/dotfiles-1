09edf22a528071814baed40360a706ade9b31b2b
szorfein <szorfein@protonmail.com>
2019-06-25 00:53:01 +1100

    add poweroff and exit at the logout screen

--- awesomewm/.config/awesome/layouts/logout.lua
+++ awesomewm/.config/awesome/layouts/logout.lua
@@ -7,13 +7,43 @@ local widgets = require("util.widgets")
 
 local pad = separators.pad
 local font_icon = "Iosevka Term Bold 63"
+local font_icon_2 = "Iosevka Term Bold 64" -- for little icon to align the text :(
 local font_text = "Iosevka Term Regular 11"
 
 local username = os.getenv("USER")
 local goodbye_text = "Goodbye " .. username:sub(1,1):upper() .. username:sub(2)
 local goodbye_widget = widgets.create_text(goodbye_text, "#999999", "Iosevka Term Bold  20")
 
---exit,
+-- {{{ Poweroff part
+local poweroff_command = function() 
+  awful.spawn.with_shell("poweroff")
+  keygrabber.stop(exit_screen_grabber)
+end
+
+local poweroff_icon = widgets.create_text("⭘", "#a22277", font_icon)
+local poweroff_text = widgets.create_text("Poweroff", "#aaaaaa", font_text)
+local poweroff = widgets.box("vertical", poweroff_icon, poweroff_text)
+poweroff:buttons(gears.table.join(
+  awful.button({ }, 1, function() 
+    poweroff_command()
+  end)
+))
+-- {{{ END Poweroff part
+
+-- {{{ Exit part
+local exit_command = function() 
+  awesome.quit()
+end
+
+local exit_icon = widgets.create_text("ﴙ", "#ff7777", font_icon_2)
+local exit_text = widgets.create_text("Exit", "#aaaaaa", font_text)
+local exit = widgets.box("vertical", exit_icon, exit_text)
+exit:buttons(gears.table.join(
+  awful.button({ }, 1, function() 
+    exit_command()
+  end)
+))
+-- {{{ END Exit part
 
 -- {{{ Lock part
 local lock_command = function() 
@@ -21,7 +51,7 @@ local lock_command = function()
   exit_screen_hide()
 end
 
-local lock_icon = widgets.create_text("", "#aaaaaa", font_icon)
+local lock_icon = widgets.create_text("", "#7777ff", font_icon)
 local lock_text = widgets.create_text("Lock", "#aaaaaa", font_text)
 local lock = widgets.box("vertical", lock_icon, lock_text)
 lock:buttons(gears.table.join(
@@ -35,7 +65,7 @@ lock:buttons(gears.table.join(
 local screen_width = awful.screen.focused().geometry.width
 local screen_height = awful.screen.focused().geometry.height
 
-exit_screen = wibox({ x = 0, y = 0, visible = false, ontop = true, type = "dock", width = screen_width, height = screen_height, bg = "#0000008f" })
+exit_screen = wibox({ x = 0, y = 0, visible = false, ontop = true, type = "dock", width = screen_width, height = screen_height, bg = "#0000004f" })
 
 -- keylogger
 local exit_screen_grabber
@@ -50,6 +80,12 @@ function exit_screen_show()
     if event == "release" then return end
     if key == 'Escape' or key == 'q' or key == 'x' then
       exit_screen_hide()
+    elseif key == 'p' then
+      poweroff_command()
+    elseif key == 'l' then
+      lock_command()
+    elseif key == 'e' then
+      exit_command()
     end
   end)
   exit_screen.visible = true
@@ -81,8 +117,10 @@ exit_screen:setup {
       nil,
       {
         --{
-          --exit,
-          pad(3),
+          poweroff,
+          pad(10),
+          exit,
+          pad(10),
           lock,
           layout = wibox.layout.fixed.horizontal
         --},
