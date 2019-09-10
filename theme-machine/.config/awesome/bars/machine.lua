6e9676bab754206348381df3e82fde5ce7551812
szorfein <szorfein@protonmail.com>
2019-09-10 07:29:53 -0800

    last settings on the theme-machine

--- theme-machine/.config/awesome/bars/machine.lua
+++ theme-machine/.config/awesome/bars/machine.lua
@@ -2,54 +2,17 @@ local awful = require("awful")
 local gtable = require("gears.table")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
-local helpers = require("helpers")
-local dpi = require('beautiful').xresources.apply_dpi
+local dpi = beautiful.xresources.apply_dpi
 local separators = require('util.separators')
 local widget = require('util.widgets')
 
 -- widgets load
-local hostname = require("widgets.hostname")
 local text_taglist = require("taglists.connected")
-local scrot = require("widgets.scrot")
 local pad = separators.pad
-local arrow = separators.arrow_left
-
--- gradient colors (dark to light)
-local g0 = beautiful.grey_dark
-local g1 = beautiful.grey
-local g2 = beautiful.primary_dark
-local g3 = beautiful.secondary_dark
-
--- {{{ Add a background to the widgets
-local tor = require("widgets.button_tor")
-local my_tor = widget.bg( g1, tor )
-
-local network = require("widgets.network")
-local my_network = widget.bg( g2, network_widget )
-
-local wifi_str = require("widgets.wifi_str")
-local my_wifi_str = widget.bg( g3, wifi_str_widget )
-
 local mpc = require("widgets.button_only_mpc")
-
-local volume = require("widgets.volume")
-local my_volume = widget.bg( g2, volume_widget )
-
 local change_theme = require("widgets.button_change_theme")
-
-local ram = require("widgets.ram")
-local my_ram = widget.bg( g1, ram_widget )
-
-local battery = require("widgets.battery")
-local my_battery = widget.bg( g3, battery_widget )
-
-local mail = require("widgets.mail")
-local my_email = widget.bg( g2, mail )
-
-local date = require("widgets.date")
-local my_date = widget.bg( g1, date_widget )
-
 local layoutbox = require("widgets.layout")
+local scrot = require("widgets.scrot")
 
 local sidebar_arrow = widget.create_button("#daeda1", " >")
 sidebar_arrow:buttons(gtable.join(
